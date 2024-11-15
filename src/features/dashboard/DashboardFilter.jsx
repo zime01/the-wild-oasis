aafa2a8b423720794c41f9e513fb8f6905aaa818
Hazim Zimić <zime1921@gmail.com>
2024-11-15 10:05:36 +0100

    filter

--- src/features/dashboard/DashboardFilter.jsx
+++ src/features/dashboard/DashboardFilter.jsx
@@ -3,11 +3,11 @@ import { Filter } from "../../ui/Filter";
 function DashboardFilter() {
   return (
     <Filter
-      filterField="last"
-      options={[
-        { value: "90", label: "Last 90 days" },
-        { value: "30", label: "Last 30 days" },
+    filterField="last"
+    options={[
         { value: "7", label: "Last 7 days" },
+        { value: "30", label: "Last 30 days" },
+        { value: "90", label: "Last 90 days" },
       ]}
     />
   );
